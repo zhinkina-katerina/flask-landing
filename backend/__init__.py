4c17c5e053d70db2e1fad1f0a8b337b4691a8637
zhinkina.katerina <katerina.zhinkina@gmail.com>
2022-06-29 17:09:42 +0300

    feat(deploy): prepare to deploy - add flask-cors - add Procfile - edit requirements.txt - edit app settings - add proxy to package.json

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,10 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from .config import DevelopmentConfig
 from flask_ckeditor import CKEditor
+from flask_cors import CORS
+import os
 
-
-app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+app = Flask(__name__, static_folder=os.path.join(BASE_DIR, 'frontend/build'))
 ckeditor = CKEditor(app)
+CORS(app)
 app.config.from_object(DevelopmentConfig)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
--- backend/views.py
+++ backend/views.py
@@ -3,15 +3,22 @@ from backend import app
 from .models import Project
 import json
 from .request_handler import RequestHandler
+from flask.helpers import send_from_directory
+from flask_cors import cross_origin
 
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 
 @app.route("/api/get_portfolio_items", methods=["GET"])
+@cross_origin
 def get_portfolio_items():
     projects = Project.query.all()
     return json.dumps(Project.serialize_list(projects))
 
 
 @app.route("/api/send_email", methods=["POST"])
+@cross_origin
 def send_email():
     request_handler = RequestHandler()
     result = (eval(request.form.to_dict()['params']))
